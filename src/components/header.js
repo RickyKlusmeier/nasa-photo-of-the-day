7c90762b320a07f4a5d190a5ff3b7efcc03f2ce9
Ricky Klusmeier <rickyklusmeier@Rickys-MacBook-Pro.local>
2021-03-17 19:39:07 -0600

    MVP

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Links from './links'
+
+export default function Header() {
+return (
+<div className="App-header">
+    <Links />
+</div>
+)} 
---
+++ src/components/links.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Links () {
+    return (
+        <>
+        <a className = 'App-link' href = '#'>Home</a>
+        <a className = 'App-link' href = '#'>Previous Picture Of The Day</a>
+        <a className = 'App-link' href = '#'>Contact</a>
+        </>
+    )
+}
